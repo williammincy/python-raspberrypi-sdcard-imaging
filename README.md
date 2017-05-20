bdfed75af12c24f759f4d81f2aff5929398bf49e
Wlliam Mincy <will@wmdigital.com>
2017-05-20 14:30:55 -0400

    + Added disk formatting as an option pre-imaging + Updated README

--- README.md
+++ README.md
@@ -14,12 +14,14 @@ The requirements are very small to run this. You'll need:
   - SD card
   - 6GB free disk space
 
+It's also worth mentioning that you should be able to `sudo` since some of the commands will require this to work properly.
+
 ### Instructions
 
-Once you've cloned/downloaded the repository you'll need to also install the `pip` package
+Once you've cloned/downloaded the repository you'll need to also install the `pip` packages needed to run the script
 
 ```
-   pip install click
+   pip install click hurry.filesize
 ```
 
 At this point you should prepare your SD card that you want to use by formatting it. Once done, you can initiate the script: 
@@ -28,7 +30,7 @@ At this point you should prepare your SD card that you want to use by formatting
    python rpi-setup.py
 ```
 
-The script takes you through selecting the disk from those available and picking the RPi disk image to install. More information about the difference between the two can be found on the [Raspberry Pi website](https://www.raspberrypi.org/downloads/raspbian/). From there it will download the appropriate disk image and install it onto the SD card.
+The script takes you through selecting the disk from those available and picking the RPi disk image to install. More information about the difference between the two can be found on the [Raspberry Pi website](https://www.raspberrypi.org/downloads/raspbian/). From there it will download the appropriate disk image, format the SD card if you enabled that option, and complete the installation.
 
 After successfully installing the disk image onto the SD card the script will clean up after itself, removing the zip and disk image files from your computer.
 
--- rpi-setup.py
+++ rpi-setup.py
@@ -2,99 +2,162 @@ import plistlib
 import urllib2
 import os
 import sys
+from hurry.filesize import size
 import click
 import subprocess
 import zipfile
 import datetime, time
 
 outputfilename = "rpi"
-dir_path = os.getcwd()
 raspbian = [{"url":"https://downloads.raspberrypi.org/raspbian_latest","descr":"(Full version of OS)"},{"url":"https://downloads.raspberrypi.org/raspbian_lite_latest","descr":"(Lite version of OS)"}]
 
-def print_choose_disk():
-    dc = 1
-    disks = pl['AllDisksAndPartitions']
-    actualdisks = []
-    click.secho("Enter number of volume you want to use for Raspberry Pi installation:", fg='green')
-    for disk in disks:
-        if "Partitions" in disk:
-            actualdisks.append(disk)
-            partitions = disk["Partitions"]
-            click.secho(" "+" "+str(dc)+": "+disk["DeviceIdentifier"]+" "+"("+partitions[0]["Content"]+")", fg='green')
-            dc += 1
-    rawresult = input()
-    try:
-        result = int(rawresult)
-    except:
-        result = 0
+def chunk_report(bytes_so_far, chunk_size, total_size):
+   percent = float(bytes_so_far) / total_size
+   percent = round(percent*100, 2)
+   sys.stdout.write("Downloaded %d of %d bytes (%0.2f%%)\r" % 
+       (bytes_so_far, total_size, percent))
 
-    if result < 1 or result > dc:
-        sys.stdout.flush()
-        click.secho("Sorry, "+str(rawresult)+" is not a valid choice", fg='red')
-        return print_choose_disk()
-    else:
-        sys.stdout.flush()
-        return actualdisks[result-1]
+   if bytes_so_far >= total_size:
+      sys.stdout.write('\n')
+
+def chunk_read(response, chunk_size=8192, report_hook=None):
+   total_size = response.info().getheader('Content-Length').strip()
+   total_size = int(total_size)
+   bytes_so_far = 0
+   imagefile = ''
+
+   while 1:
+      chunk = response.read(chunk_size)
+      bytes_so_far += len(chunk)
+
+      if not chunk:
+         break
+
+      imagefile += chunk
+      if report_hook:
+         report_hook(bytes_so_far, chunk_size, total_size)
+   
+   return imagefile 
+
+def print_choose_disk():
+    try:
+        dc = 1
+        disks = pl['AllDisksAndPartitions']
+        actualdisks = []
+        click.secho("Enter number of volume you want to use for Raspberry Pi installation:", fg='green')
+        for disk in disks:
+            partitionInfo = " (???)"
+            if "Partitions" in disk:
+                partitionInfo = " ("+disk["Partitions"][-1]["VolumeName"]+")"
+            actualdisks.append(disk)
+            click.secho(" "+" "+str(dc)+": "+disk["DeviceIdentifier"]+" - "+size(disk["Size"])+partitionInfo, fg='green')
+            dc += 1
+        rawresult = raw_input()
+        try:
+            result = int(rawresult)
+        except:
+            result = 0
+
+        if result < 1 or result > dc:
+            click.secho("Sorry, "+str(rawresult)+" is not a valid choice", fg='red')
+            return print_choose_disk()
+        else:
+            return actualdisks[result-1]
+    except KeyboardInterrupt:
+        sys.stdout.write('\r'+"")
+        sys.exit(0)
 
 def print_choose_diskimage():
-    click.secho("Choose your version of Raspbian:", fg='green')
-    rc = 1
-    for img in raspbian:
-        click.secho(" "+" "+str(rc)+": "+img["url"]+" "+img["descr"], fg='green')
-        rc += 1
-    rawresult = input()
     try:
-        result = int(rawresult)
-    except:
-        result = 0
-    
-    if result < 1 or result > 2:
-        sys.stdout.flush()
-        click.secho("Sorry, "+str(rawresult)+" is not a valid choice", fg='red')
-        return print_choose_diskimage()
-    else:
-        return raspbian[result-1]
+        click.secho("Choose your version of Raspbian:", fg='green')
+        rc = 1
+        for img in raspbian:
+            click.secho(" "+" "+str(rc)+": "+img["url"]+" "+img["descr"], fg='green')
+            rc += 1
+        rawresult = input()
+        try:
+            result = int(rawresult)
+        except:
+            result = 0
+        
+        if result < 1 or result > 2:
+            sys.stdout.flush()
+            click.secho("Sorry, "+str(rawresult)+" is not a valid choice", fg='red')
+            return print_choose_diskimage()
+        else:
+            return raspbian[result-1]
+    except KeyboardInterrupt:
+        sys.stdout.write('\r'+"")
+        sys.exit(0)
 
-def install_rpi_image(diskpath, diskimage):
+def print_format_disk():
+    click.secho("Would you like to format the disk? y/n", fg='green')
+    try:
+        rawresult = raw_input()
+        result = str(rawresult).lower()
+        if result == "y":
+            return 1
+        elif result == "n":
+            return 0
+        else:
+            sys.stdout.write("Invalid option ")
+            print_format_disk()
+    except KeyboardInterrupt:
+        sys.stdout.write('\r'+"")
+        sys.exit(0)
+    except:
+        sys.stdout.write("Invalid option ")
+        print_format_disk()
+
+def install_rpi_image(diskpath, shouldFormatDisk, diskimage):
     click.secho("Unmounting disk", fg='green')
     subprocess.call(["diskutil","unmountDisk","/dev/"+diskpath])
-    click.secho("Imaging SD card, this may take 15 to 20 minutes depeding on the size of your SD card", fg='green')
+    if shouldFormatDisk == 1:
+        click.secho("Formatting SD card, please wait a moment", fg='green')
+        subprocess.call(["sudo","diskutil","partitionDisk","/dev/"+diskpath,"1 MBR","free","\"%noformat%\"","100%"])
+    click.secho("Imaging SD card, this may take 15 minutes or more depeding on the size of your SD card", fg='green')
     subprocess.call(["sudo","dd","bs=1m","if="+diskimage,"of=/dev/"+diskpath])
     click.secho("Cleaning up", fg='green')
-    subprocess.Popen("rm -Rf "+diskimage)
+    subprocess.call(["rm","-Rf",diskimage])
     click.secho("Complete", fg='green')
 
 def dl_rpi_image(url, filename):
     data = None
     click.secho("Downloading Raspbian Image, this may take a few minutes", fg='green')
     req = urllib2.urlopen(url)
+    imagefile = chunk_read(req, report_hook=chunk_report)
     fo = open(filename+".zip", "wb")
-    fo.write(req.read())
+    fo.write(imagefile)
     fo.close()
-    click.secho("Unzipping archive", fg='green')
+    click.secho("Unzipping archive, this may take a moment", fg='green')
+    dimagename = ''
     with zipfile.ZipFile(filename+".zip", "r") as z:
-        z.extractall(filename+".img")
+        dimage = [item for item in z.infolist() if item.filename.endswith('.img')][-1]
+        dimagename = dimage.filename
+        z.extract(dimage)
+    subprocess.call(["mv",dimagename,outputfilename+".img"])
     subprocess.call(["rm","-Rf",filename+".zip"])
 
 if __name__ == "__main__":
-    try:
-        startTime = datetime.datetime.now()
-        click.secho("Starting at "+startTime.strftime("%I:%M %p"), fg='blue')
+    # try:
+    startTime = datetime.datetime.now()
+    click.secho("Starting at "+startTime.strftime("%I:%M %p"), fg='blue')
+    
+    output = subprocess.check_output(["diskutil","list","-plist"])
+    pl = plistlib.readPlistFromString(output)
+    if len(pl['AllDisksAndPartitions']) > 0:
+        disk = print_choose_disk()
+        shouldFormatDisk = print_format_disk()
+        img = print_choose_diskimage()
+        dl_rpi_image(img["url"], outputfilename)
+        install_rpi_image(disk['DeviceIdentifier'], shouldFormatDisk, outputfilename+".img")
+        endTime = datetime.datetime.now()
+        difftime = endTime - startTime
         
-        output = subprocess.check_output(["diskutil","list","-plist"])
-        pl = plistlib.readPlistFromString(output)
-        if len(pl['AllDisksAndPartitions']) > 0:
-            disk = print_choose_disk()
-            img = print_choose_diskimage()
-            dl_rpi_image(img["url"], outputfilename)
-            install_rpi_image(disk['DeviceIdentifier'], outputfilename+".img")
-            endTime = datetime.datetime.now()
-            difftime = endTime - startTime
-            
-            click.secho("Completed at "+endTime.strftime("%I:%M %p"), fg='blue')
-            click.secho("Process took "+str(difftime), fg='blue')
-        else:
-            print "No disks or volumes found"
-    except:
-        sys.stdout.write('\r'+"Cancelling\n\n") 
-        sys.exit(0)
+        click.secho("Completed at "+endTime.strftime("%I:%M %p"), fg='blue')
+        click.secho("Process took "+str(difftime), fg='blue')
+    else:
+        print "No disks or volumes found"
+    # except:
+    #     sys.stdout.write('\r'+"Cancelling\n\n") 
+    #     sys.exit(0)
